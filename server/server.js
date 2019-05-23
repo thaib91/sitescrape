e11c4d3bc926163c59bc3ddd5b544086ac4db189
Thai Bui <thaibui@Thais-MBP.fios-router.home>
2019-05-23 16:22:37 -0400

    tried with forloop

--- server/server.js
+++ server/server.js
@@ -8,27 +8,60 @@ const siteUrl = "https://toronto.iabc.com/about/pic/pic-member-list/";
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 926});
     await page.goto(siteUrl);
+    await page.waitForSelector("body > div.site-container > div.site-inner > div > main > article > div")
+
+    const siteData = await page.evaluate(()=>{
+        const dataNodeList = document.querySelectorAll('body > div.site-container > div.site-inner > div > main > article > div')
+        const nameList = document.querySelectorAll('div.su-column-inner.su-clearfix > strong > a')
+        const pageData = [];
+
+        for (let i = 0; i < dataNodeList.length; i++){
+            pageData[i] = {
+                name: nameList[i].innerText
+            }
+        }
+        console.log(pageData)
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //getting details of member list
 
-    const siteData = await page.evaluate(()=>{
-        const pageData = [];
-        //get page elements
-        const pageElms = document.querySelectorAll('body > div.site-container > div.site-inner');
+    // const siteData = await page.evaluate(()=>{
+    //     const pageData = [];
+    //     //get page elements
+    //     const pageElms = document.querySelectorAll('body > div.site-container > div.site-inner > div > main > article');
 
-        //parse data from elements
-        pageElms.forEach((element)=>{
-            const pageJson = {};
-            try{
-                pageJson.name = element.querySelector('.su-column-inner.su-clearfix').innerText;
+    //     //parse data from elements
+    //     pageElms.forEach((element)=>{
+    //         const pageJson = {};
+    //         try{
+    //             pageJson.name = element.querySelector('div.su-column-inner.su-clearfix').innerText;
 
 
-            }catch(err){
-                console.log(err)
-            }
-            pageData.push(pageJson)
-        }) ;
-        return pageData;
-    })
-    console.dir(siteData)
+    //         }catch(err){
+    //             console.log(err)
+    //         }
+    //         pageData.push(pageJson)
+    //         console.log(pageData)
+    //     }) ;
+    //     return pageData;
+    // })
+    // console.dir(siteData)
+
 })();
