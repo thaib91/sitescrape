c4179b5057a3620806e0eab8d1583276d0193d3c
Thai Bui <thaibui@Thais-MBP.fios-router.home>
2019-05-24 09:38:45 -0400

    Issue with fetch of data

--- server/server.js
+++ server/server.js
@@ -10,18 +10,18 @@ const siteUrl = "https://toronto.iabc.com/about/pic/pic-member-list/";
     await page.goto(siteUrl);
     await page.waitForSelector("body > div.site-container > div.site-inner > div > main > article > div")
 
-    const siteData = await page.evaluate(()=>{
-        const dataNodeList = document.querySelectorAll('body > div.site-container > div.site-inner > div > main > article > div')
-        const nameList = document.querySelectorAll('div.su-column-inner.su-clearfix > strong > a')
-        const pageData = [];
+    // const siteData = await page.evaluate(()=>{
+    //     const dataNodeList = document.querySelectorAll('body > div.site-container > div.site-inner > div > main > article > div')
+    //     const nameList = document.querySelectorAll('div.su-column-inner.su-clearfix > strong > a')
+    //     const pageData = [];
 
-        for (let i = 0; i < dataNodeList.length; i++){
-            pageData[i] = {
-                name: nameList[i].innerText
-            }
-        }
-        console.log(pageData)
-    });
+    //     for (let i = 0; i < dataNodeList.length; i++){
+    //         pageData[i] = {
+    //             name: nameList[i].innerText
+    //         }
+    //     }
+    //     console.log(pageData)
+    // });
 
 
 
@@ -42,26 +42,26 @@ const siteUrl = "https://toronto.iabc.com/about/pic/pic-member-list/";
 
     //getting details of member list
 
-    // const siteData = await page.evaluate(()=>{
-    //     const pageData = [];
-    //     //get page elements
-    //     const pageElms = document.querySelectorAll('body > div.site-container > div.site-inner > div > main > article');
+    const siteData = await page.evaluate(()=>{
+        const pageData = [];
+        //get page elements
+        const pageElms = document.querySelectorAll('body > div.site-container > div.site-inner > div > main > article');
 
-    //     //parse data from elements
-    //     pageElms.forEach((element)=>{
-    //         const pageJson = {};
-    //         try{
-    //             pageJson.name = element.querySelector('div.su-column-inner.su-clearfix').innerText;
+        //parse data from elements
+        pageElms.forEach((element)=>{
+            const pageJson = {};
+            try{
+                pageJson.name = element.querySelectorAll('div .su-row').innerText;
 
 
-    //         }catch(err){
-    //             console.log(err)
-    //         }
-    //         pageData.push(pageJson)
-    //         console.log(pageData)
-    //     }) ;
-    //     return pageData;
-    // })
-    // console.dir(siteData)
+            }catch(err){
+                console.log(err)
+            }
+            pageData.push(pageJson)
+            console.log(pageData)
+        }) ;
+        return pageData;
+    })
+    console.dir(siteData)
 
 })();
